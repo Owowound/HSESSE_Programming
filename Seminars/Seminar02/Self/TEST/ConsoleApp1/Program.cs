5eb335dd5c92d8458f369b0d49a412e7f535492a
Vlad <vlddom5@mail.ru>
2023-09-25 18:59:37 +0300

    TEST

---
+++ Seminars/Seminar02/Self/TEST/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { ///Sem05_task02
+            string a;
+            int t1;
+            double t2;
+            int a1 = 0, a2 = 0, a3 = 0;
+
+            do
+            {
+                a = Console.ReadLine();
+                
+                if (double.TryParse(a, out t2))
+                {
+                    //Console.WriteLine(a);
+                    if (int.TryParse(a, out t1))
+                    {
+                        //Console.WriteLine(a);
+                        a1++;
+                    }
+                    else
+                    {
+                        //Console.WriteLine(a);
+                        a2++;
+                    }
+                }
+                else
+                {
+                    a3++;
+                }
+            } while (a != "0" && a != "-0");
+
+            Console.WriteLine($"{a1} {a2} {a3}");
+
+
+        }
+    }
+}
---
+++ Seminars/Seminar02/Self/TEST/ConsoleApp11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, ans = 0, s = 0;
+            x = double.Parse(Console.ReadLine());
+
+            while (s <= x)
+            {
+                ans = ans + s * s * 0.001;
+                //Console.WriteLine($"{ans}  {s * s} ");
+                s = s + (0.001);
+            }
+            Console.WriteLine(ans);
+        }
+    }
+}
